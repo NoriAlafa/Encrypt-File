f7132ee5a2ec71e54d289812a2901b8c31d3650b
NoriAlafa <49442695+NoriAlafa@users.noreply.github.com>
2020-03-16 14:59:00 +0700

    Add files via upload

---
+++ Sandi AN.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string alfabet{"abcdefghijklmABCDEFGHIJKLM"};
+    string keys{"nopqrstuvwxyzNOPQRSTUVWXYZ"};
+    char word;
+    
+    string secret_massage{};
+    cout<<"Enter the secret massage"<<endl;
+    getline(cin,secret_massage);
+
+    string encrypted_massage{};
+    cout<<"Your encrypted massage"<<endl;
+    for(char word:secret_massage){
+        size_t position=alfabet.find(word);
+        if(position != string::npos){
+            char new_character{keys.at(position)};
+            encrypted_massage+=new_character;
+
+        }else{
+            encrypted_massage+=word;
+        }
+    }
+   cout<<encrypted_massage;
+
+
+}
+
+
